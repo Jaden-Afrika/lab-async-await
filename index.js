34d5826ee3cc32854b488f0176c05922416ae860
afrika <jaden.afrika@student.moringaschool.com>
2026-04-15 12:50:03 +0300

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your code here!
+
+const postList = document.getElementById('post-list');
+function displayPosts(posts) {
+posts.forEach(post => {
+    const li = document.createElement('li');
+    const h1 = document.createElement('h1'); 
+    const p = document.createElement('p'); 
+
+    h1,textContent = post.title;
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+    postList.appendChild(li);
+});
+}
+async function getPosts() {
+try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();     
+    displayPosts(data);
+} catch (error) {
+    console.error('Error fetching posts:', error);
+}
+}
+
+   getPosts();
